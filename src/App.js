7854cf3810d34b4c8548a8bff16229bce29a2a03
Sheraz Haider <sheraz5006@gmail.com>
2020-07-20 02:34:06 +0500

    navbar, users, and useritem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
