454d3aa64d6590c5aca7b89103dad735b7054701
Sheraz Haider <sheraz5006@gmail.com>
2020-08-23 01:49:43 +0500

    Updated About Page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,9 +7,10 @@ const About = () => {
       <p style={{marginBottom: '10px'}}>This app is called GitHub Developers. Utilizes GitHub public api to sreach for any github user and look at their information.</p>
       <p>Implementation:</p>
       <ul style={{listStyle: 'circle', margin: '10px 40px', }}>
-        <li>Developed as a SPA using React</li>
-        <li>Used Context Api (alternative to redux) to manage app level state</li>
-        <li>Implemented routing to navigate through pages and show 404 when page not found</li>
+        <li>Developed as a SPA using React.</li>
+        <li>Used Context Api (alternative to redux) to manage app level state.</li>
+        <li>Used Github public api to get developersa and their information.</li>
+        <li>Implemented routing to navigate through pages and show 404 when page not found.</li>
       </ul>
       <small><i>Copyright &copy; 2020</i></small>
     </Fragment>
